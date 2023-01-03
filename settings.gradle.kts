0b3b8921b2934992531ba26ef40f8a2e66f4199f
vitor <vgc_silva@hotmail.com>
2023-01-03 18:29:27 +0000

    Setup gradle project
    Starting exploring Gradle
    
    [Ticket: 3]

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories.gradlePluginPortal()
+/*    repositories.mavenCentral()
+    repositories.google()
+    repositories.maven("http://my.location/repo"){
+        credentials.username = "..."
+        credentials.password = "..."
+    }
+    includeBuild("gradle/plugins")*/
+}
+
+dependencyResolutionManagement {
+    repositories.mavenCentral()
+/*    repositories.google()
+    repositories.maven("http://my.location/repo") {
+        credentials.username = "..."
+        credentials.password = "..."
+    }
+
+    includeBuild("../my-other-project")*/
+}
+
+rootProject.name = "Specify a project-name"
+
+include("app")
+include("business-logic")
+include("data-model")
