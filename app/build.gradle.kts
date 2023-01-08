1027bd81ae4c17589ae25841101c2a5948ffef75
vitor <vgc_silva@hotmail.com>
2023-01-08 18:45:14 +0000

    Compose an Application - add Dependencies
    Add the dependencies for all the modules and setup the java main class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,14 @@ plugins {
     id("my-application")
 }
 
+application {
+    mainClass.set("com.example.MyApplication")
+}
+
+dependencies {
+    implementation(project(":data-model"))
+    implementation(project(":business-logic"))
+}
 /*
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(11))
--- business-logic/build.gradle.kts
+++ business-logic/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     id("my-java-library")
 }
 
+dependencies{
+    implementation(project(":data-model"))
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+}
 /*
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(11))
